8bc25f9f8212c29f8741cbf7f88043a674369bce
Vinicius <v1n1c1u5_1@hotmail.com>
2019-06-23 13:19:41 -0300

    Criar Procedure Migration

---
+++ Eiji/Fontes/Exame.App/src/Exame.Infra/Migrations/Procedure.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Data.Entity.Migrations;
+using System.Text;
+
+namespace Exame.Infra.Migrations
+{
+    class Procedure : DbMigration
+    {
+        public override void Up()
+        {
+            StringBuilder storedProcedureCode = new StringBuilder();
+
+            storedProcedureCode.Append("CREATE PROCEDURE ListarMovimentoProduto AS" + Environment.NewLine);
+            storedProcedureCode.Append("SELECT" + Environment.NewLine);
+            storedProcedureCode.Append("mm.DAT_MES as Mes," + Environment.NewLine);
+            storedProcedureCode.Append("mm.COD_PRODUTO as CodigoProduto," + Environment.NewLine);
+            storedProcedureCode.Append("p.DES_PRODUTO as DescricaoProduto," + Environment.NewLine);
+            storedProcedureCode.Append("mm.NUM_LANCAMENTO as NumeroLancamento," + Environment.NewLine);
+            storedProcedureCode.Append("mm.DES_DESCRICAO as DescricaoMovimento," + Environment.NewLine);
+            storedProcedureCode.Append("mm.VAL_VALOR as Valor" + Environment.NewLine);
+            storedProcedureCode.Append("FROM MOVIMENTO_MANUAL mm" + Environment.NewLine);
+            storedProcedureCode.Append("INNER JOIN PRODUTO p ON mm.COD_PRODUTO = p.COD_PRODUTO" + Environment.NewLine);
+            storedProcedureCode.Append("ORDER BY mm.DAT_MES, mm.DAT_ANO, mm.NUM_LANCAMENTO" + Environment.NewLine);
+
+            this.Sql(storedProcedureCode.ToString());
+        }
+
+        public override void Down()
+        {
+            this.Sql("DROP PROCEDURE ListarMovimentoProduto ");
+        }
+    }
+}
